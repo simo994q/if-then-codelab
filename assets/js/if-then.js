0064c4a68aa6d1dae5e6707976bbdc40f7990506
simo994q <simonkn1@hotmail.com>
2023-02-07 12:52:24 +0100

    if then

--- assets/js/if-then.js
+++ assets/js/if-then.js
@@ -8,33 +8,43 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B.
 
-if ( /*..dit expression Her*/ ) {
+if (A == B) {
     console.log('statement true!');
 }
 
 
 //opgave 2: skrive en If then statement der vurderer om A er større end B.
 
-
+if (A > B) {
+    console.log('statement true!');
+}
 
 
 //opgave 3: Skriv en If then statement der vurderer om A er mindre end eller lig med B
 
-
+if (A <= B) {
+    console.log('statement true!');
+}
 
 
 
 //opgave 4: Skriv en If then statement der bliver FALSE når du sammenligner B og C.
 
-
+if (B === C) {
+    console.log('statement true!');
+}
 
 
 
 //opgave 5: Skriv en If then statement der bliver TRUE når du sammenligner B og C.
 
-
+if (B == C) {
+    console.log('statement true!');
+}
 
 
 //opgave 6: Skriv en If then statement der sammenligner B = C og E = A hvis B=C skal den blive TRUE. 
 
-
+if (B == C && E == A || B == C) {
+    console.log('statement true!');
+}
