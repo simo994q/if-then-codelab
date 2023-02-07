4750aae06183c2a165797f4f39f26a15f9337da2
simo994q <simonkn1@hotmail.com>
2023-02-07 12:58:38 +0100

    if tehn

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -8,14 +8,16 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B eller A er lig med D.
 
-if ( /*..dit expression Her*/ ) {
+if (A == B || A == D) {
     console.log('statement true!');
 }
 
 
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
 
-
+if (A > B && A < E) {
+    console.log('statement true!');
+}
 
 
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
@@ -23,5 +25,8 @@ if ( /*..dit expression Her*/ ) {
 
 
 
-
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
+
+if (!(F)) {
+    console.log(F);
+}
