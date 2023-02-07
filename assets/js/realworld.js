ddfae83a89ede6540201e0dee6955e8239207010
simo994q <simonkn1@hotmail.com>
2023-02-07 14:49:26 +0100

    if then :)

--- assets/js/realworld.js
+++ assets/js/realworld.js
@@ -2,7 +2,8 @@
 /* du må både ændre koden her, og i din HTML, hvis det er nødvendigt.. */
 
 
-/*Opgave 1: skriv en validering af svar i koden herunder. resultatet skal vises i myAnsverFeedbackElement når bruger trykker på myAnsverButton .
+/*Opgave 1: skriv en validering af svar i koden herunder. resultatet skal vises i 
+myAnsverFeedbackElement når bruger trykker på myAnsverButton.
 der et korrekte svar:
 nej 
 */
@@ -12,26 +13,63 @@ const myAnsverInput = document.getElementById('answerOne');
 const myAnsverButton = document.getElementById('answerButton');
 const myAnsverFeedbackElement = document.getElementById('answerFeedback');
 
-myAnsverButton.addEventListener('click', () => {
+myAnsverButton.addEventListener('click', (e) => {
+      e.preventDefault()
+      let selectedValue = myAnsverInput.value
+      if (selectedValue == 'true') {
+        showResult ('yes')    
+      } else {
+        showResult ('nope')      
+      }
+      function showResult (res) {
+        myAnsverFeedbackElement.innerHTML = res
+      }
+
 
     /* din kode her.  husk at skrive view funktion der viser om svaret er korrekt i myAnsverFeedbackElement, brug evt. innerHTML*/
 
 });
 
+
+
 /*Opgave 2: udkommenter opgave 1, og omskriv nu din funktion til to korrekte svar:
 nej
 ja i disney film
 */
+// Det er bare HTML'en der skal ændres?
 
 
-/*opgave 3: skriv en kode der valider formen med følgende elementer: (denne opgave er på extreme niveau) 
+/*opgave 3: skriv en kode der valider formen med følgende elementer: (denne opgave er på extreme 
+  niveau) 
 
-- korrekt e-mail type. brug funktionen "validateEmail(minEmail) til at checke en string med den indtastede email
-- navnet skal være længere end 3 bogstaver, med realtime validering (brug keyup til at checke, og .lengt til at finde længden)
+- korrekt e-mail type. brug funktionen "validateEmail(minEmail) til at checke en string med den 
+indtastede email
+- navnet skal være længere end 3 bogstaver, med realtime validering (brug keyup til at checke, 
+  og .lengt til at finde længden)
 - alder skal være større end 12.
- du må gerne bare consol.logge de forskellige tests i konsollen, men hvis du kan må du gerne lave feedback i elementet formValideringResult.
+ du må gerne bare consol.logge de forskellige tests i konsollen, men hvis du kan må du gerne 
+ lave feedback i elementet formValideringResult.
 */
 
+const myName = document.getElementById('myName');
+const myEmail = document.getElementById('myEmail');
+const myAge = document.getElementById('myAge');
+const submitButton = document.getElementById('submitButton');
+
+
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (myName.value.length > 3) {
+    console.log('Name valid');
+  }
+  if (validateEmail(myEmail.value)) {
+    console.log('Email valid');
+  }
+  if (myAge.value > 12) {
+    console.log('Age valid');
+  }
+})
 
 
 
